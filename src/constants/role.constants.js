c539dfc31ff09fb138f230e00fe7073a98de6d44
abhisheksingh01-ai <abhishek.ai.learn@gmail.com>
2026-08-20 21:08:30 +0530

    Remove all files

--- src/constants/role.constants.js
+++
@@ -1,8 +0,0 @@
-export const ROLES = Object.freeze({
-  SUPER_ADMIN: "super_admin",
-  COMPANY_ADMIN: "company_admin",
-  SUB_ADMIN: "sub_admin",
-  MANAGER: "manager",
-  TEAM_HEAD: "team_head",
-  AGENT: "agent",
-});
